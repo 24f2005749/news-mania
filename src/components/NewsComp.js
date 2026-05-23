e41bbbded8ebedc2692a28483b4bf4416543e2b3
Rohit Singh <silvr@Rohits-MacBook-Air.local>
2026-05-23 23:43:55 +0530

    fixed cors with proxy

--- src/components/NewsComp.js
+++ src/components/NewsComp.js
@@ -32,19 +32,19 @@ export class NewsComp extends Component {
 
     async componentDidMount() {
         this.setState({loading: true})
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `/api/news?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading:false });
+        this.setState({ articles: parsedData.articles || [], totalResults: parsedData.totalResults, loading:false });
 
     }
 
     pagePrevious = async () => {
         this.setState({loading: true})
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `/api/news?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ page: this.state.page - 1, articles: parsedData.articles , loading:false});
+        this.setState({ page: this.state.page - 1, articles: parsedData.articles || [] , loading:false});
 
     }
 
@@ -53,10 +53,10 @@ export class NewsComp extends Component {
         if (!((this.state.page + 1) > Math.ceil(this.state.totalResults / this.pageSize))) {
 
             this.setState({loading: true})
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `/api/news?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             let data = await fetch(url);
             let parsedData = await data.json();
-            this.setState({ page: this.state.page + 1, articles: parsedData.articles , loading:false});
+            this.setState({ page: this.state.page + 1, articles: parsedData.articles || [] , loading:false});
 
         }
     }
@@ -69,7 +69,7 @@ export class NewsComp extends Component {
                 <h1 className='text-center'> NewsMania - {this.props.category}</h1>
                 {this.state.loading && <Spinner/>}
                 <div className="row" >
-                    {!this.state.loading && this.state.articles.map((element) => (
+                    {!this.state.loading && (this.state.articles || []).map((element) => (
 
                         <div className="col-md-4" key={element.url}>
                             <NewsItem title={element.title ? element.title : "No title given"} desc={element.description ? element.description : "No Description available"} imageUrl={element.urlToImage ? element.urlToImage : "https://images.macrumors.com/t/MfyaJZJZunvOLaOpkgFC_y-sr_k=/2500x/article-new/2021/05/Memorial-Day-Deals-2021-Apple.jpg"} url={element.url} author={element.author} date={element.publishedAt} />
